1db716bb64ccbd483982128571f06dc73ba1fba7
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-28 12:24:28 +0200

    setup environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express, { Application } from 'express';
+import { NODE_ENV, PORT } from './config/env';
 
 const app: Application = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
 );
---
+++ src/config/env.ts
@@ -0,0 +1,2 @@
+export const PORT = parseInt(process.env.PORT || '5000', 10);
+export const NODE_ENV = process.env.NODE_ENV || 'development';
