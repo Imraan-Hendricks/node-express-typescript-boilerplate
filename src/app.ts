36497dbd4e5d6e4dd9bcf3297c62062c062a6eec
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-28 12:32:00 +0200

    set static directories

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application } from 'express';
+import path from 'path';
 import { NODE_ENV, PORT } from './config/env';
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () =>
   console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
